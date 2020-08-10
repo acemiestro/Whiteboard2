752cea07e8a740982e5f0e55bcbf08a4207be457
Paarth Sethi <paarthsethi@gmail.com>
2020-08-10 18:17:14 +0530

    pencil and eraser

---
+++ canvas.js
@@ -0,0 +1,39 @@
+// press mouse
+let isPenDown = false;
+board.addEventListener("mousedown", function(e) {
+    // begin path
+    ctx.beginPath();
+    // move to mouse pointers location
+    let x = e.clientX;
+    let y = e.clientY;
+    let top = getLocation();
+    y = Number(y) - top
+    ctx.moveTo(x, y);
+    console.log("Mouse down")
+    isPenDown = true;
+})
+
+// on move
+board.addEventListener("mousemove", function(e) {
+    if (isPenDown) {
+        console.log("Mouse move")
+        let x = e.clientX;
+        let y = e.clientY;
+        let top = getLocation();
+        y = Number(y) - top;
+        ctx.lineTo(x, y);
+        // stroke
+        ctx.stroke();
+    }
+});
+
+window.addEventListener("mouseup", function() {
+    // close Path
+    console.log("Mouse up")
+    isPenDown = false;
+});
+
+function getLocation() {
+    let { top } = board.getBoundingClientRect();
+    return top;
+}
